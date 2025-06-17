b704f0d29c4a9eb3bb1644389cc8a38aba1c5a7d
majharul <majharuljony163@gmail.com>
2025-06-17 12:56:49 +0600

    Initial setup

---
+++ tasks/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("Welcome to the Task Management System!")
+
+def show_task(request):
+    return HttpResponse("Here is a list of tasks.")
