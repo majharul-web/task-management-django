ea07d35906443c9c8582ded0c0c2e16704e4a89e
majharul <majharuljony163@gmail.com>
2025-06-22 11:08:46 +0600

    use static

--- tasks/views.py
+++ tasks/views.py
@@ -8,3 +8,6 @@ def manager_dashboard(request):
 
 def user_dashboard(request):
     return render(request, 'dashboard/user-dashboard.html')
+
+def test(request):
+    return render(request, 'test.html')
