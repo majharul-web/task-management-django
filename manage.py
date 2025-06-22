2e27421f16340358e62021eeb15c713098be93d8
majharul <majharuljony163@gmail.com>
2025-06-22 16:20:06 +0600

    PSQL database setup and create task table

--- manage.py
+++ manage.py
@@ -24,6 +24,9 @@ if __name__ == '__main__':
 # create a virtual environment: python -m venv venv
 # linux venv activate: source venv/bin/activate
 # windows venv activate: venv\Scripts\activate
+# create requirements.txt: pip freeze > requirements.txt
 # install dependencies: pip install -r requirements.txt
 # see dependencies: pip list
+# make migrations: python manage.py makemigrations
+# migrate database: python manage.py migrate
 # run-server: python manage.py runserver
