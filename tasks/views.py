51cf9b64386870703d3bf66711e9a65aca2b4c14
majharul-web <majharuljony163@gmail.com>
2025-06-24 22:34:09 +0600

    use filter

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from tasks.forms import TaskModelForm
-from tasks.models import Task
+from tasks.models import Task,TaskDetail
+from datetime import date
 
 # Create your views here.
 
@@ -29,13 +30,19 @@ def create_task(request):
 
 def view_tasks(request):
     tasks = Task.objects.all()  # Fetch all tasks from the database
-    specific_tasks = Task.objects.get(id=1)  # Example of fetching a specific task by ID
+    specific_tasks = Task.objects.get(pk=5)  # Example of fetching a specific task by ID
     first_task = Task.objects.first()  # Fetch the first task in the database
     last_task = Task.objects.last()  # Fetch the last task in the database
+    tasks_by_status = Task.objects.filter(status='PENDING')  # Example of filtering tasks by status
+    tasks_by_due_date = Task.objects.filter(due_date=date.today())  # Example of filtering tasks by due date
+    tasks_by_priority = TaskDetail.objects.exclude(priority='H')  # Example of filtering tasks by priority
     context = {
         'tasks': tasks,
         'specific_task': specific_tasks,
         'first_task': first_task,
         'last_task': last_task,
+        'tasks_by_status': tasks_by_status,
+        'tasks_by_due_date': tasks_by_due_date,
+        'tasks_by_priority': tasks_by_priority,
     }
     return render(request, 'view-tasks.html', context)
