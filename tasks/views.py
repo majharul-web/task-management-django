7ea7cf69ac3f565b59a8b992a300535dd100b2ed
majharul <majharuljony163@gmail.com>
2025-06-25 12:15:37 +0600

    use aggregate

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from tasks.forms import TaskModelForm
-from tasks.models import Task,TaskDetail
+from tasks.models import Task,TaskDetail,Project
 from datetime import date
-from django.db.models import Q
+from django.db.models import Q,Count
 
 # Create your views here.
 
@@ -31,6 +31,7 @@ def create_task(request):
 
 def view_tasks(request):
     tasks = Task.objects.all()  # Fetch all tasks from the database
+    count_all_tasks = Task.objects.aggregate(count=Count('id'))  # Count all tasks
     specific_tasks = Task.objects.get(pk=5)  # Example of fetching a specific task by ID
     first_task = Task.objects.first()  # Fetch the first task in the database
     last_task = Task.objects.last()  # Fetch the last task in the database
@@ -44,8 +45,11 @@ def view_tasks(request):
     
     check_is_exists = Task.objects.filter(title__icontains='urgent').exists()  # Check if any task contains 'urgent' in the title
 
+    task_count_in_project=Project.objects.annotate(task_count=Count('task')).order_by('-task_count')  # Count tasks in each project
+
     context = {
         'tasks': tasks,
+        'count_all_tasks': count_all_tasks['count'],
         'specific_task': specific_tasks,
         'first_task': first_task,
         'last_task': last_task,
@@ -55,5 +59,6 @@ def view_tasks(request):
         'filter_task_using_and_contains': filter_task_using_and_contains,
         'filter_task_using_or': filter_task_using_or,
         'check_is_exists': check_is_exists,
+        'task_count_in_project': task_count_in_project,
     }
     return render(request, 'view-tasks.html', context)
