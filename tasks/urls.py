6b42bdc6b4bd4abd66ba915aa5ed15a379e8eb52
majharul <majharuljony163@gmail.com>
2025-06-29 14:51:29 +0600

    task updated

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from tasks.views import manager_dashboard,user_dashboard,create_task,view_tasks,related_tasks
+from tasks.views import manager_dashboard,user_dashboard,create_task,update_task,view_tasks,related_tasks
 urlpatterns = [
     path('manager-dashboard/',manager_dashboard,name='manager-dashboard'),
     path('user-dashboard/',user_dashboard,name='user-dashboard'),
     path('create-task/', create_task,name='create-task'),  
+    path('update-task/<int:id>/', update_task,name='update-task'),  
     path('view-tasks/', view_tasks,name='view-tasks'),
     path('related-tasks/', related_tasks,name='related-tasks'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -60,6 +60,31 @@ def create_task(request):
     }
     return render(request, 'task-form.html', context)
 
+def update_task(request,id):
+    task = Task.objects.get(pk=id)  # Fetch the task to be updated
+    task_form = TaskModelForm(instance=task)
+    if(task.details):
+        task_detail_form = TaskDetailModelForm(instance=task.details)
+    
+
+    if(request.method == 'POST'): # If the request is POST, we need to process the form data
+        task_form = TaskModelForm(request.POST, instance=task)
+        task_detail_form = TaskDetailModelForm(request.POST, instance=task.details)
+        if task_form.is_valid() and task_detail_form.is_valid():
+            task = task_form.save()
+            task_detail = task_detail_form.save(commit=False)  
+            task_detail.task = task  
+            task_detail.save()  
+            messages.success(request, "Task updated successfully!")
+            return redirect('update-task', id=task.id)
+            # return HttpResponse("Task created successfully!")
+        
+    context= {
+        'task_form': task_form,
+        'task_detail_form': task_detail_form,
+    }
+    return render(request, 'task-form.html', context)
+
 
 
 def related_tasks(request):
